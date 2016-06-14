0bfa2bb5db9e0fa71596e19158b8b5e6818986a6
thomas@mutzl.com <thomas@mutzl.com>
2016-06-14 16:31:11 +0200

    Filtering

--- YANApp.PCL/ViewModels/AllNotesViewModel.cs
+++ YANApp.PCL/ViewModels/AllNotesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace YANApp.PCL.ViewModels
 {
+	using System;
 	using System.Collections.ObjectModel;
 	using System.Linq;
 
@@ -19,19 +20,51 @@
 		{
 			this.dataService = dataService;
 			this.settings = settings;
+
+
+			PropertyChanged += (sender, args) =>
+			{
+				if (args.PropertyName == nameof(SearchTerm)
+				 || args.PropertyName == nameof(FromDate)
+				 || args.PropertyName == nameof(ToDate))
+				{
+					LoadData();
+				}
+			};
 		}
+
+
+		public Note SelectedNote { get; set; }
 
 		public void LoadData()
 		{
 			var notes = dataService.GetAllNotes();
-			notes = settings.IsSortAscending 
-						? notes.OrderBy(n => n.CreatedAt) 
+
+			//if (!string.IsNullOrEmpty(SearchTerm)) notes = notes.Where(n => n.Title.ToLower().Contains(SearchTerm) || n.Content.ToLower().Contains(SearchTerm));
+			//if (FromDate.HasValue)                 notes = notes.Where(n => n.CreatedAt >= FromDate.Value.Date);  
+			//if (ToDate.HasValue)                   notes = notes.Where(n => n.CreatedAt < ToDate.Value.Date.AddDays(1));
+
+			notes = notes.Where(n => (!FromDate.HasValue || n.CreatedAt >= FromDate.Value.Date)
+								  && (!ToDate.HasValue || n.CreatedAt < ToDate.Value.Date.AddDays(1))
+								  && (string.IsNullOrEmpty(SearchTerm) || n.Title.ToLower().Contains(SearchTerm) || n.Content.ToLower().Contains(SearchTerm)));
+
+			notes = settings.IsSortAscending
+						? notes.OrderBy(n => n.CreatedAt)
 						: notes.OrderByDescending(n => n.CreatedAt);
+
+
 			notes = notes.Take(settings.NumberOfNotes);
 			Notes = new ObservableCollection<Note>(notes);
 		}
 
+		public bool IsEmptyList => Notes.Count == 0;
+
 		public ObservableCollection<Note> Notes { get; set; }
+
+		public string SearchTerm { get; set; }
+
+		public DateTime? FromDate { get; set; }
+		public DateTime? ToDate { get; set; }
 
 	}
 }
---
+++ YANApp/Converters/DateTimeToDateTimeOffsetConverter.cs
@@ -0,0 +1,24 @@
+﻿
+namespace YANApp.Converters
+{
+	using System;
+
+	using Windows.UI.Xaml.Data;
+	public class DateTimeToDateTimeOffsetConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			var date = value as DateTime?;
+			if (!date.HasValue) return null;
+
+			return new DateTimeOffset(date.Value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			var date = value as DateTimeOffset?;
+
+			return date?.DateTime;
+		}
+	}
+}
